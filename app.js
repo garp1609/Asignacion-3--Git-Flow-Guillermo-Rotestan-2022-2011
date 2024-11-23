5b2eb6344b5ecb905cc3777a5bb8f3844653c567
garp1609 <152009968+garp1609@users.noreply.github.com>
2024-11-22 22:46:03 -0400

    Enviar al formulario

--- app.js
+++ app.js
@@ -11,3 +11,32 @@ window.onload = function () {
     createRows(tasks);
     updateTaskCount(tasks);
 };
+
+// Evento al enviar el formulario
+todoForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const text = document.getElementById("text").value;
+    const date = document.getElementById("date").value;
+    const hour = document.getElementById("hour").value;
+
+    if (!title || !text || !date || !hour) {
+        Swal.fire({
+            text: "Por favor, completa todos los campos antes de agregar la tarea.",
+            icon: "warning",
+            confirmButtonColor: "#d33",
+            confirmButtonText: "OK",
+        });
+        return;
+    }
+
+    if (!todoForm.checkValidity()) {
+        event.stopPropagation();
+        todoForm.classList.add("was-validated");
+    } else {
+        const data = new FormData(this);
+        addTask(data);
+        todoForm.classList.remove("was-validated");
+    }
+});
